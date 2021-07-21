b39933e13868cfdcb67939547fb834ebfb340e98
Hamzah <h4mz1221@gmail.com>
2021-07-21 14:25:55 +0100

    first commit

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    publicPath:'./',
+    pages:{
+        index:{
+            entry:'src/main.js',
+            title:'Index'
+        },
+            intersections:{
+                entry:'src/Intersections/main.js',
+                title:'Intersections',
+                
+            },
+            parallelopiped_volume:{
+                entry:'src/Parallelopiped volume/main.js',
+                title:'Parallelopiped volume',
+                
+            },
+    }
+}
